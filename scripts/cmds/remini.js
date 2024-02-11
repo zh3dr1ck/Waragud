384538a6b91d686299716d8856241dbaf13f2020
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-02-11 19:40:37 +0800

    Update cmds

---
+++ scripts/cmds/remini.js
@@ -0,0 +1,66 @@
+const axios = require('axios');
+const fs = require('fs-extra');
+
+module.exports = {
+  config: {
+    name: "remini",
+    aliases: [],
+    author: "Hazeyy/kira", // Hindi ito collab, ako kasi nag-convert :>
+    version: "69",
+    cooldowns: 5,
+    role: 0,
+    shortDescription: {
+      en: "remini filter"
+    },
+    longDescription: {
+      en: "remini filter"
+    },
+    category: "image",
+    guide: {
+      en: "{p}{n} [reply to an img]"
+    }
+  },
+
+  onStart: async function ({ api, event }) {
+    const args = event.body.split(/\s+/).slice(1); // Use slice to skip the first element
+    const { threadID, messageID, messageReply } = event;
+    const pathie = __dirname + `/tmp/zombie.jpg`;
+
+    // Check if there's a message reply and if it has attachments
+    if (!messageReply || !messageReply.attachments || !(messageReply.attachments[0] || args[0])) {
+      api.sendMessage("┐⁠(⁠￣⁠ヘ⁠￣⁠)⁠┌ | Must reply to an image or provide an image URL.", threadID, messageID);
+      return;
+    }
+
+    // Determine the photo URL from the reply or command arguments
+    const photoUrl = messageReply.attachments[0] ? messageReply.attachments[0].url : args.join(" ");
+
+    // Check if a valid photo URL is present
+    if (!photoUrl) {
+      api.sendMessage("┐⁠(⁠￣⁠ヘ⁠￣⁠)⁠┌ | Must reply to an image or provide an image URL.", threadID, messageID);
+      return;
+    }
+
+    api.sendMessage("⊂⁠(⁠・⁠﹏⁠・⁠⊂⁠) | Please wait...", threadID, async () => {
+      try {
+        const response = await axios.get(`https://code-merge-api-hazeyy01.replit.app/api/try/remini?url=${encodeURIComponent(photoUrl)}`);
+        const processedImageURL = response.data.image_data;
+
+        // Fetch the processed image
+        const enhancedImage = await axios.get(processedImageURL, { responseType: "arraybuffer" });
+
+        // Save the processed image to a temporary file
+        fs.writeFileSync(pathie, enhancedImage.data);
+
+        // Send the enhanced image as a reply
+        api.sendMessage({
+          body: "<⁠(⁠￣⁠︶⁠￣⁠)⁠> | Image Enhanced.",
+          attachment: fs.createReadStream(pathie)
+        }, threadID, () => fs.unlinkSync(pathie), messageID);
+      } catch (error) {
+        // Handle errors gracefully
+        api.sendMessage(`(⁠┌⁠・⁠。⁠・⁠)⁠┌ | Api Dead...: ${error}`, threadID, messageID);
+      }
+    });
+  }
+};
