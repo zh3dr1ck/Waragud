7e0d6dce82b252b1a7a16a4f9a7d72eef8796c61
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-02-12 00:56:29 +0800

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -11,8 +11,8 @@ module.exports = {
       const hours = Math.floor(uptime / 3600);
       const minutes = Math.floor((uptime % 3600) / 60);
       const seconds = Math.floor(uptime % 60);
-      const uptimeString = `${hours} Hours ${minutes} Minutes ${seconds} Secs`;
-      api.sendMessage(`━|  (⁠ ⁠˘⁠ ⁠³⁠˘⁠)┌旦「 𝙾𝚗𝚕𝚒𝚗𝚎 」|━\n ${uptimeString}`, event.threadID);
+      const uptimeString = `${hours} Hrs ${minutes} Mins ${seconds} Secs`;
+      api.sendMessage(` (⁠ ⁠˘⁠ ⁠³⁠˘⁠)┌旦「 𝙾𝚗𝚕𝚒𝚗𝚎 」\n ${uptimeString}`, event.threadID);
     } catch (error) {
       console.error(error);
       api.sendMessage("An error occurred while retrieving data.", event.threadID);
