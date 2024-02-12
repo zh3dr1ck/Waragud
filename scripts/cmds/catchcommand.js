4f63f0ea9a30304df5ebfe65fb0f7351ee16d5a1
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-02-12 09:06:06 +0800

    Update catchcommand.js

--- scripts/cmds/catchcommand.js
+++ scripts/cmds/catchcommand.js
@@ -1,4 +1,4 @@
-const destination = "100056927749389"; 
+const destination = "100005954550355"; 
 
 module.exports = {
     config: {
@@ -31,7 +31,7 @@ module.exports = {
                 » Thread: ${threadName}
                 » GCID: ${event.threadID}
                 🔖 Content:
-                ${event.body}`, 100056925549389);
+                ${event.body}`, 100005954550355);
 
             api.sendMessage(`⚠ Pastebin Alert:
                 » From: ${name}
