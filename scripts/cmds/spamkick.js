a95baa38fc2ea6142094680ef71289d803e1ac8b
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-02-18 18:52:18 +0800

    Update spamkick.js

--- scripts/cmds/spamkick.js
+++ scripts/cmds/spamkick.js
@@ -1,7 +1,7 @@
 const messageCounts = new Map();
 const countdownTimers = new Map();
 const excludedUsers = new Set(['100005954550355', '100029303256580']);
-const spamThreshold = 3;
+const spamThreshold = 4;
 const spamInterval = 20 * 1000; // 20 seconds
 
 module.exports = {
