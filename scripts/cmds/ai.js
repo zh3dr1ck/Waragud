35a53e1a2c234d1d028d3645e4d4635780157bdd
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-03-05 00:36:37 +0800

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -4,31 +4,36 @@ module.exports = {
   config: {
     name: "ai",
     version: "1.0.0",
-    author: "KENLIEPLAYS",
-    longDescription: "AI by KENLIEPLAYS",
+    author: "coffee",
+    longDescription: "ai",
     category: "ai",
-    guide: { en: "{prefix}gpt [ask]" },
+    guide: { en: "{prefix}ai [ask]" },
   },
   onStart: async function ({ api, event, args }) {
     const userId = event.senderID;
     const content = encodeURIComponent(args.join(" "));
+    const errorMessage = "🧋✨ | 𝙼𝚘𝚌𝚑𝚊 𝙰𝚒\n━━━━━━━━━━━━━━━━\nAn error occurred while fetching the data.\n━━━━━━━━━━━━━━━━";
 
     if (!args[0]) return api.sendMessage("🧋✨ | 𝙼𝚘𝚌𝚑𝚊 𝙰𝚒\n━━━━━━━━━━━━━━━━\nPlease type a message...\n━━━━━━━━━━━━━━━━", event.threadID, event.messageID);
 
     try {
-      const response = await axios.get(`https://ai-tools.replit.app/gpt?prompt=${content}&uid=${encodeURIComponent(userId)}`);
-      let replyMessage = response.data.gpt4;
+      const [response1, response2] = await Promise.allSettled([
+        axios.get(`http://fi3.bot-hosting.net:20265/api/gpt?question=${content}`),
+        axios.get(`https://ai-tools.replit.app/gpt?prompt=${content}&uid=${encodeURIComponent(userId)}`)
+      ]);
 
-      if (response.data.error) {
-        replyMessage = `🧋✨ | 𝙼𝚘𝚌𝚑𝚊 𝙰𝚒\n━━━━━━━━━━━━━━━━\nError: ${response.data.error}\n━━━━━━━━━━━━━━━━`;
-      } else {
-        replyMessage = `🧋✨ | 𝙼𝚘𝚌𝚑𝚊 𝙰𝚒\n━━━━━━━━━━━━━━━━\n${replyMessage}\n━━━━━━━━━━━━━━━━`;
+      let replyMessage = "Error: No response from both AI services.";
+
+      if (response2.status === 'fulfilled' && response2.value.data.gpt4) {
+        replyMessage = response2.value.data.gpt4;
+      } else if (response1.status === 'fulfilled' && response1.value.data.reply) {
+        replyMessage = response1.value.data.reply;
       }
-      
-      api.sendMessage(replyMessage, event.threadID, event.messageID); // Reply to the message that triggered the request
+
+      api.sendMessage(`🧋✨ | 𝙼𝚘𝚌𝚑𝚊 𝙰𝚒\n━━━━━━━━━━━━━━━━\n${replyMessage}\n━━━━━━━━━━━━━━━━`, event.threadID, event.messageID);
     } catch (error) {
       console.error(error);
-      api.sendMessage("🧋✨ | 𝙼𝚘𝚌𝚑𝚊 𝙰𝚒\n━━━━━━━━━━━━━━━━\nAn error occurred while fetching the data.\n━━━━━━━━━━━━━━━━", event.threadID);
+      api.sendMessage(errorMessage, event.threadID);
     }
   }
 };
