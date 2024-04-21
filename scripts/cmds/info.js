3917ade78970d21b75233db354508a8bc3f76233
zh3dr1ck <167666853+zh3dr1ck@users.noreply.github.com>
2024-04-21 21:22:25 +0800

    Create info.js

---
+++ scripts/cmds/info.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const moment = require('moment-timezone');
+
+module.exports = {
+	config: {
+		name: "info",
+		version: "1.0",
+		author: "charlie",
+		countDown: 20,
+		role: 0,
+		shortDescription: { vi: "", en: "" },
+		longDescription: { vi: "", en: "" },
+		category: "owner",
+		guide: { en: "" },
+		envConfig: {}
+	},
+	onStart: async function ({ message }) {
+		const botName = "Charlie | AI";
+		const botPrefix = "/";
+		const authorName = "Charlie";
+		const ownAge = "17";
+		const teamName = "Neggas team";
+		const authorFB = "https://www.facebook.com/incognitotab.dev";
+		const urls = JSON.parse(fs.readFileSync('cliff.json'));
+		const link = urls[Math.floor(Math.random() * urls.length)];
+		const now = moment().tz('Asia/Jakarta');
+		const date = now.format('MMMM Do YYYY');
+		const time = now.format('h:mm:ss A');
+		const uptime = process.uptime();
+		const seconds = Math.floor(uptime % 60);
+		const minutes = Math.floor((uptime / 60) % 60);
+		const hours = Math.floor((uptime / (60 * 60)) % 24);
+		const days = Math.floor(uptime / (60 * 60 * 24));
+		const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+
+		message.reply({
+			body: `《  Bot & Owner Info 》
+\Name: ${botName}
+\Bot Prefix: ${botPrefix}
+\owner: ${authorName}
+\Age : ${ownAge}
+\Facebook: ${authorFB}
+\Date: ${date}
+\Time: ${time}
+\Team: ${teamName}
+\Uptime: ${uptimeString}
+\===============`,
+			attachment: await global.utils.getStreamFromURL(link)
+		});
+	},
+	onChat: async function ({ event, message, getLang }) {
+		if (event.body && event.body.toLowerCase() === "info") {
+			this.onStart({ message });
+		}
+	}
+};
